b46644edb1db12197bd8c1b3cdb411e0b89484cc
V-nyang <l64906947+nftug@users.noreply.github.com>
2025-11-02 03:35:04 +0900

    fix: Update useMutation to return TaskResult and handle errors

--- AttendanceRecord.Presentation/Common/Hooks/UseMutationExtensions.fs
+++ AttendanceRecord.Presentation/Common/Hooks/UseMutationExtensions.fs
@@ -1,28 +1,40 @@
 namespace Avalonia.FuncUI
 
-open System.Threading.Tasks
 open System.Runtime.CompilerServices
+open FsToolkit.ErrorHandling
 
 type UseMutationResult<'arg, 'ret> =
-    { MutateTask: 'arg -> Task<'ret>
+    { MutateTask: 'arg -> TaskResult<'ret, string>
       Mutate: 'arg -> unit
-      IsPending: IReadable<bool> }
+      IsPending: IReadable<bool>
+      Error: IReadable<string option> }
 
 [<Extension>]
 type __UseMutationExtensions =
     [<Extension>]
-    static member useMutation(ctx: IComponentContext, mutateFunc: ('arg -> Task<'ret>)) =
+    static member useMutation(ctx: IComponentContext, mutateFunc: ('arg -> TaskResult<'ret, string>)) =
         let isPending = ctx.useState false
+        let error = ctx.useState<string option> None
 
         let mutateTask =
             fun (param: 'arg) ->
                 task {
                     isPending.Set true
+                    error.Set None
+
                     let! result = mutateFunc param
+
                     isPending.Set false
-                    return result
+
+                    match result with
+                    | Ok v -> return Ok v
+                    | Error e ->
+                        printfn $"Mutation error: {e}"
+                        error.Set(Some e)
+                        return Error e
                 }
 
         { MutateTask = mutateTask
           Mutate = mutateTask >> ignore
-          IsPending = isPending |> ctx.usePassedRead }
+          IsPending = isPending |> ctx.usePassedRead
+          Error = error |> ctx.usePassedRead }

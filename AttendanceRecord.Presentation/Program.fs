f6ee9d149c9e964e69bfde770279ce7c19504f1d
V-nyang <l64906947+nftug@users.noreply.github.com>
2025-11-03 21:03:43 +0900

    refactor: Add AppStyles module for button and toggle button styling

--- AttendanceRecord.Presentation/Program.fs
+++ AttendanceRecord.Presentation/Program.fs
@@ -2,12 +2,9 @@
 
 open Avalonia
 open Avalonia.Controls.ApplicationLifetimes
-open Avalonia.Controls.Primitives
 open Avalonia.Themes.Fluent
 open Avalonia.FuncUI.Hosts
-open Avalonia.Styling
 open Avalonia.Controls
-open Avalonia.Layout
 open Material.Icons.Avalonia
 open Avalonia.Controls.Notifications
 open AttendanceRecord.Presentation.Layout.Components
@@ -29,25 +26,10 @@ type MainWindow(services: ServiceContainer) as this =
 type App() =
     inherit Application()
 
-    let getMyStyles () =
-        Styles()
-        |> fun s ->
-            let buttonStyle = Style(fun x -> x.OfType<Button>())
-            buttonStyle.Add(Setter(Button.HorizontalContentAlignmentProperty, HorizontalAlignment.Center))
-            buttonStyle.Add(Setter(Button.VerticalContentAlignmentProperty, VerticalAlignment.Center))
-            s.Add buttonStyle
-
-            let toggleButtonStyle = Style(fun x -> x.OfType<ToggleButton>())
-            toggleButtonStyle.Add(Setter(Button.HorizontalContentAlignmentProperty, HorizontalAlignment.Center))
-            toggleButtonStyle.Add(Setter(Button.VerticalContentAlignmentProperty, VerticalAlignment.Center))
-            s.Add toggleButtonStyle
-
-            s
-
     override this.Initialize() =
         this.Styles.Add(FluentTheme())
         this.Styles.Add(MaterialIconStyles null)
-        this.Styles.Add(getMyStyles ())
+        this.Styles.Add(AppStyles())
 
     override this.OnFrameworkInitializationCompleted() =
         let services = ServiceContainer.create ()

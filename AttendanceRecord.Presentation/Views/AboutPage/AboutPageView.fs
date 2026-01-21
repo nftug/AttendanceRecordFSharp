0c0b0aa4eb27ba2d4dfadb4e3bc8954fe2654768
V-nyang <l64906947+nftug@users.noreply.github.com>
2026-01-22 06:14:59 +0900

    feat: Improve AboutPageView by adding author and year information

--- AttendanceRecord.Presentation/Views/AboutPage/AboutPageView.fs
+++ AttendanceRecord.Presentation/Views/AboutPage/AboutPageView.fs
@@ -2,12 +2,21 @@ namespace AttendanceRecord.Presentation.Views.AboutPage
 
 open type NXUI.Builders
 open NXUI.Extensions
+open System
 open System.Reflection
 open AttendanceRecord.Presentation.Utils
 
 module AboutPageView =
    let create () : Avalonia.Controls.Control =
-      let version = Assembly.GetEntryAssembly().GetName().Version.ToString()
+      let entryAssembly = Assembly.GetEntryAssembly()
+      let version = entryAssembly.GetName().Version.ToString()
+      let year = DateTime.Now.Year
+
+      let author =
+         entryAssembly.GetCustomAttributes<AssemblyCompanyAttribute>()
+         |> Seq.tryHead
+         |> Option.map _.Company
+         |> Option.defaultValue "Unknown Author"
 
       StackPanel()
          .HorizontalAlignmentCenter()
@@ -19,5 +28,11 @@ module AboutPageView =
                .FontWeightBold()
                .HorizontalAlignmentCenter()
                .Margin(0.0, 0.0, 0.0, 30.0),
-            TextBlock().Text($"Version {version}").FontSize(24.0).HorizontalAlignmentCenter()
+            TextBlock().Text($"Version {version}").FontSize(24.0).HorizontalAlignmentCenter(),
+            TextBlock()
+               .Text($"Copyright © {year} {author}")
+               .FontSize(16.0)
+               .Opacity(0.7)
+               .Margin(0.0, 30.0, 0.0, 0.0)
+               .HorizontalAlignmentCenter()
          )
